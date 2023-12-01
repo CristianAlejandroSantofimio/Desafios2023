01de3abccf6a906bd920683d57f79920502be8e6
Cris <alejandro.santofimioa@gmail.com>
2023-12-01 16:18:38 -0500

    pruebaCommitEste

--- src/main/java/paquetes_java/Main.java
+++ src/main/java/paquetes_java/Main.java
@@ -1,8 +1,29 @@
 package main.java.paquetes_java;
+
+import java.util.Scanner;
+
 public class Main  {
     public static void main(String[] args) {
-        System.out.println("hola mundo!!!!");
-        System.out.println("hola");
+
+        String [][] matrizTexto = new String[3][3];
+        llenarMatriz(matrizTexto);
+
 
     }
+
+    public static void llenarMatriz(String [][] matrizTexto) {
+
+        Scanner valor = new Scanner(System.in);
+        for( int i=0; i< matrizTexto.length ;i++)
+        {
+            for ( int j= 0 ; j < matrizTexto[0].length; j++)
+            {
+
+                System.out.println("ingrese el valor de esta posicion : ");
+                matrizTexto[i][j] = valor.next();
+            }
+        }
+    }
+
+
 }
