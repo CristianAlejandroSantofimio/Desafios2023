344be81c747a85af986f92c26377048f1388ba81
Cris <alejandro.santofimioa@gmail.com>
2023-12-01 14:41:41 -0500

    pruebaCommit

---
+++ src/main/java/paquetes_java/Main.java
@@ -0,0 +1,8 @@
+package main.java.paquetes_java;
+public class Main  {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("hola");
+
+    }
+}
